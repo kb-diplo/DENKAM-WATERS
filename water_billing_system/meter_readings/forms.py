d15c00c268a11c82af8a016aba4c5b22b01f4670
LARRY <tingzlarry@gmail.com>
2025-03-30 09:52:27 +0300

    Create MeterReadingForm with date input widget

--- water_billing_system/meter_readings/forms.py
+++ water_billing_system/meter_readings/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import MeterReading
 
 class MeterReadingForm(forms.ModelForm):
+    reading_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    
     class Meta:
         model = MeterReading
         fields = ['customer', 'reading_date', 'reading_value', 'notes']
