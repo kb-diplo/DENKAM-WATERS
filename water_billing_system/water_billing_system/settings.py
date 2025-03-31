d80c32cc9f205da23917d0906be8fa2dd59011f4
LARRY <tingzlarry@gmail.com>
2025-03-31 13:51:06 +0300

    Configure project settings

--- water_billing_system/water_billing_system/settings.py
+++ water_billing_system/water_billing_system/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+w#8oicp1ecn7q-2!f*=rgs^q#$a#ex(7c2p11+xix#*u9+n3$'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv('django-insecure-+w#8oicp1ecn7q-2!f*=rgs^q#$a#ex(7c2p11+xix#*u9+n3$')
+DEBUG = os.getenv('DEBUG') == 'True'
+
 
 ALLOWED_HOSTS = []
-
-AUTH_USER_MODEL = 'accounts.User'
 
 
 # Application definition
@@ -45,7 +48,18 @@ INSTALLED_APPS = [
     'billing',
     'payments',
     'reports',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+LOGIN_URL = 'accounts:login'
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'accounts:login'
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -114,7 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
